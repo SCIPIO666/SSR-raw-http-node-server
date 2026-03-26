2a7ea0565d04c270775524655e74d530eb0cdfda
SCIPIO666 <eaphoney@gmail.com>
2026-03-26 10:11:57 +0300

    bare bones ssr impleemntation

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
-console.log('server is on');
+const http=require('http');
+const fs=require('fs');
+const PORT = 3000;
+const path = require("path");
+
+const server=http.createServer((req,res)=>{
+
+console.log(req.url)
+let filePath='';
+
+//routes
+    if(req.url==="/"){
+        filePath=path.join(__dirname,"views","index.html")
+    }else if(req.url==="/contact"){
+        filePath = path.join(__dirname, "views", "contact-me.html");
+    }else if(req.url==="/about"){
+        filePath = path.join(__dirname, "views", "about.html");
+    }else{
+        filePath = path.join(__dirname, "views", "404.html");
+        res.statusCode = 404;
+    }
+
+fs.readFile(filePath,(err,data)=>{
+    if(err){
+        res.writeHead(500);
+        res.end("server error");
+        return;
+    }
+    res.writeHead(200,{"Content-Type": "text/html"})
+    res.end(data)
+})
+
+
+})
+
+server.listen(PORT,"localhost",()=>{
+    console.log(`Server running at http://localhost:${PORT}`)
+})
